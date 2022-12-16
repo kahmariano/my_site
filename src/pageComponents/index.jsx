fb8ed563f1a5efd3f23e9eba2fe3fd0ebe8bca72
kahmariano <kmariano138@gmail.com>
2022-12-16 08:48:47 -0300

    estilizaçao

--- src/pageComponents/index.jsx
+++ src/pageComponents/index.jsx
@@ -1,43 +1,74 @@
-import { HomeWrapper } from './styles'
+import { HomeWrapper, Imagediv, Conteudo, Links } from './styles'
 import CreateGlobalStyle from '../styles/global'
 import Image from 'next/image'
 export function HomePage() {
   return (
     <HomeWrapper>
       <CreateGlobalStyle />
-      <div>
+      <Imagediv>
         <Image
           alt="kah mariano"
           src="/kahmariano.jpeg"
           width={200}
           height={200}
-          // blurDataURL="/kahmariano.jpeg"
+          blurDataURL="/kahmariano.jpeg"
           style={{
-            borderRadius: '50%'
+            borderRadius: '40%'
           }}
         />
-      </div>
+      </Imagediv>
 
-      <h1>
-        Karina Mariano
-        <style>
-          @import
-          url('https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap')
-        </style>
-      </h1>
-      <p>Atualmente barbeira, me ariscando em programação</p>
-      <a
-        href="https://www.linkedin.com/in/karina-mariano-278a2b175/"
-        target="blank"
-      >
-        <img src="" alt="linkedin" />
-      </a>
-      <a href="https://www.instagram.com/kahbarber/" target="blank">
-        <img src="" alt="instagran" />
-      </a>
-      <a href="https://github.com/kahmariano/" target="blank">
-        <img src="" alt="github" />
-      </a>
+      <Conteudo>
+        <h1>
+          Karina Mariano
+          <style>
+            @import
+            url('https://fonts.googleapis.com/css2?family=La+Belle+Aurore&display=swap')
+          </style>
+        </h1>
+        <p>Atualmente barbeira, me ariscando em programação</p>
+      </Conteudo>
+      <Links>
+        <a
+          href="https://www.linkedin.com/in/karina-mariano-278a2b175/"
+          target="blank"
+        >
+          <Image
+            alt="linkedin"
+            src="/linkedin.png"
+            width={30}
+            height={30}
+            blurDataURL="/linkedin.png"
+            style={{
+              borderRadius: '40%'
+            }}
+          />
+        </a>
+        <a href="https://www.instagram.com/kahbarber/" target="blank">
+          <Image
+            alt="instagram"
+            src="/instagram.png"
+            width={30}
+            height={30}
+            blurDataURL="/instagram.png"
+            style={{
+              borderRadius: '40%'
+            }}
+          />
+        </a>
+        <a href="https://github.com/kahmariano/" target="blank">
+          <Image
+            alt="github"
+            src="/github.png"
+            width={30}
+            height={30}
+            blurDataURL="/github.png"
+            style={{
+              borderRadius: '40%'
+            }}
+          />
+        </a>
+      </Links>
     </HomeWrapper>
   )
 }
--- src/pageComponents/styles.jsx
+++ src/pageComponents/styles.jsx
@@ -3,8 +3,30 @@ import styled from 'styled-components'
 export const HomeWrapper = styled.div`
   font-family: 'La Belle Aurore', cursive;
 `
-export const div = styled.div`
+export const Imagediv = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: column;
+  margin-top: 5rem;
+`
+export const Conteudo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  h1 {
+    font-size: 50px;
+  }
+  p {
+    font-size: 32px;
+    line-height: 16px;
+  }
+`
+
+export const Links = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
 `
